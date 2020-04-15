0208cd9c42b10046241e3e037541ee9bfa9b67b0
andres123dbh <60190072+andres123dbh@users.noreply.github.com>
2020-04-14 20:01:00 -0500

    Ejercicio 1 en java

--- preinforme10_BonillaAndres.py
+++ preinforme10_BonillaAndres.py
@@ -59,8 +59,8 @@ def defisit_cada_año(utilidad):
             defisit=utilidad[i+1]-utilidad[i]
             porsentaje_defisit=(defisit*100)/utilidad[i]
             if porsentaje_defisit>=0:
-                print("No hubo deficit por que no tuvieron perdidas el porsentaje de ganancia es: ",porsentaje_defisit)
+                print("No hubo deficit por que no tuvieron perdidas, el porsentaje de ganancia es: ",porsentaje_defisit)
             else:
                 print("El deficit del año fue: ",(-1*porsentaje_defisit))
 
-defisit_cada_año(utilidad)
+comparar(utilidad)
---
+++ pruebajava/preinforme10L_BonillaAndres/src/app/preinforme10L_BonillaAndres.java
@@ -0,0 +1,25 @@
+package app;
+
+public class preinforme10L_BonillaAndres {
+    public static void main(final String[] args) {
+        final int utilidades[] = new int[10];
+        utilidades[0]=27834;
+        utilidades[1]=23789;
+        utilidades[2]=30189;
+        utilidades[3]=30967;
+        utilidades[4]=32501;
+        utilidades[5]=32701;
+        utilidades[6]=31665;
+        utilidades[7]=17155;
+        utilidades[8]=4614;
+        utilidades[9]=834;
+        diferencia_promedio(utilidades);
+    }
+
+    public static void diferencia_promedio(int utilidades[]) {
+        int promedio_ultimos_años=(utilidades[8]+utilidades[9])/2;
+        int promedio_primeros_años=(utilidades[0]+utilidades[1])/2;
+        int diferencia_promedio=promedio_ultimos_años-promedio_primeros_años;
+        System.out.println("La diferencia del promedio de los dos ultimos años respecto a los dos primeros es: "+diferencia_promedio);
+    }
+}
