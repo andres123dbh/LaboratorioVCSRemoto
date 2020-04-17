b501638126e08802ba18e090c0bc062a6cf95d4d
andres123dbh <60190072+andres123dbh@users.noreply.github.com>
2020-04-16 19:40:21 -0500

    Pregunta 2 en java

--- preinforme11/java/preinforme11L_BonillaAndres/src/app/preinforme11L_BonillaAndres.java
+++ preinforme11/java/preinforme11L_BonillaAndres/src/app/preinforme11L_BonillaAndres.java
@@ -9,7 +9,10 @@ public class preinforme11L_BonillaAndres {
         {9,9.5,8.5,8,7.5,7.5,9,9,9,8.5,8,7,7,8,8.5,7.5,9,8.5,8.5,9,8.5,6.5,9,9,8},
         {60,60,20,60,60,40,60,60,60,0,78,60,30,60,60,60,60,60,60,50,40,60,40,60,50}};
 
-        promedio_precio(videojuegos);
+        double mayor = mayor_precio(videojuegos);
+        double menor = menor_precio(videojuegos);
+        System.out.println("El juego con mayor precio es " + nombre_videojuegos[10] + " que cuesta " + mayor);
+        System.out.println("El juego con menor precio es " + nombre_videojuegos[9] + " que cuesta " + menor);
     }
 
     public static void promedio_precio(double videojuegos[][]){
@@ -21,4 +24,32 @@ public class preinforme11L_BonillaAndres {
         System.out.println("El promedio del precio de los videojuegos es "+promedio+" dolares");
 
     }
+
+    public static double mayor_precio(double videojuegos[][]){
+        double mayor = 0;
+        for(int i=0;i<25;i++){
+            if (i == 0){
+                mayor = videojuegos[2][i];
+            }
+            if (videojuegos[2][i]<mayor){
+                mayor = videojuegos[2][i];
+            }
+        }
+        return mayor;
+    }
+
+    public static double menor_precio(double videojuegos[][]){
+        double menor = 0;
+        for(int i=0;i<25;i++){
+            if (i == 0){
+                menor = videojuegos[2][i];
+            }
+            if (videojuegos[2][i]>menor){
+                menor = videojuegos[2][i];
+            }
+        }
+        return menor;
+    }
+
+
 }
