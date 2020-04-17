ba45851acda404b2f0acb92d417051004061d02d
andres123dbh <60190072+andres123dbh@users.noreply.github.com>
2020-04-17 18:25:13 -0500

    Punto b:Parametros de ingresos y egresos

--- informe9/informe11_A_BonillaHiguera.py
+++ informe9/informe11_A_BonillaHiguera.py
@@ -1,10 +1,11 @@
 import numpy as np
 
-v_minimo = int(input("Digite el vaor minomo del parametro"))
-v_maximo = int(input("Digite el vaor maximo del parametro"))
-
 def generador(v_minimo,v_maximo):
     generado = np.random.randint(v_minimo,v_maximo+1, size=48).reshape(4,12)
-    print(generado)
+    return generado
 
-generador(v_minimo,v_maximo)
+ingresos = generador(100,180)
+egresos = generador(60,130)
+
+print("Ingresos: ",ingresos)
+print("Egresos: ",egresos)
