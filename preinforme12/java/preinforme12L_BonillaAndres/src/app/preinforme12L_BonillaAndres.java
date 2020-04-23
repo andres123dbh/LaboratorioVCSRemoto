0cedd11d6d9e235bf53f789bfad29e317a43c48b
andres123dbh <60190072+andres123dbh@users.noreply.github.com>
2020-04-23 13:49:30 -0500

    Ejemplo de listas en java

---
+++ preinforme12/java/preinforme12L_BonillaAndres/src/app/preinforme12L_BonillaAndres.java
@@ -0,0 +1,15 @@
+package app;
+import java.util.ArrayList;
+import java.util.List;
+
+public class preinforme12L_BonillaAndres {
+    public static void main(String[] args) throws Exception {
+        List<String> ejemploLista = new ArrayList<String>();
+        ejemploLista.add("Andres");
+        ejemploLista.add("Daniel");
+        ejemploLista.add("Hugo");
+        ejemploLista.add("Isabel");
+        ejemploLista.add("Juan");
+        System.out.println(ejemploLista);
+    }
+}
