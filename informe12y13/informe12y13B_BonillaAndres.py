24b92cc352a23172b583d7ba0a56cdfd19fb8670
andres123dbh <60190072+andres123dbh@users.noreply.github.com>
2020-05-07 21:04:02 -0500

    Parte B-punto 13:crear funcion combinar

--- informe12y13/informe12y13B_BonillaAndres.py
+++ informe12y13/informe12y13B_BonillaAndres.py
@@ -1,4 +1,12 @@
 ponderado = {"A":1,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"J":10,"Q":10,"K":10}
 simbolos = ["♥(C)","♦(D)","♣(T)","♠(P)"]
 
-print(simbolos)
+def combinar(ponderado,simbolos):
+    baraja = {}
+    for i in simbolos:
+        for a in ponderado:
+            baraja[i+a] = ponderado[a]
+    return baraja
+
+baraja = combinar(ponderado,simbolos)
+print(baraja)
