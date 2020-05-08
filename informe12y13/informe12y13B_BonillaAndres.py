8f50438979eff67963cc51939776db9c101b85a5
andres123dbh <60190072+andres123dbh@users.noreply.github.com>
2020-05-07 21:58:53 -0500

    Parte B-punto 16:crear funcion repartir

--- informe12y13/informe12y13B_BonillaAndres.py
+++ informe12y13/informe12y13B_BonillaAndres.py
@@ -26,4 +26,30 @@ baraja = revolver(baraja)
 
 cartas_jugador = []
 cartas_tallador = []
+lista_baraja = []
 
+def repartir(baraja,cartas_repartir,lista_baraja):
+    if lista_baraja:
+        None
+    else:
+        for i in baraja:
+            lista_baraja.append(i)
+    if cartas_repartir:
+        if len(cartas_repartir) >= 2:
+            numero = random.randint(0,len(lista_baraja)-1)
+            cartas_repartir.append(lista_baraja[numero])
+            lista_baraja.pop(numero)
+    else:
+        for i in range(0,2):
+            numero = random.randint(0,len(lista_baraja)-1)
+            cartas_repartir.append(lista_baraja[numero])
+            lista_baraja.pop(numero)
+    return cartas_repartir,lista_baraja
+
+
+cartas_jugador, lista_baraja = repartir(baraja,cartas_jugador,lista_baraja)
+cartas_tallador, lista_baraja = repartir(baraja,cartas_tallador,lista_baraja)
+
+print(cartas_jugador)
+print(cartas_tallador)
+print(len(lista_baraja))
