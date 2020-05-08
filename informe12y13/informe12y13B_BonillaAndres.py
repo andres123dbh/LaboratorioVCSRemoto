80d0feb89101163e5a9b8c7da2c3cba7f4bf3e0d
andres123dbh <60190072+andres123dbh@users.noreply.github.com>
2020-05-07 21:22:12 -0500

    Parte B-punto 14:crear funcion revolver

--- informe12y13/informe12y13B_BonillaAndres.py
+++ informe12y13/informe12y13B_BonillaAndres.py
@@ -1,3 +1,5 @@
+import random
+
 ponderado = {"A":1,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"J":10,"Q":10,"K":10}
 simbolos = ["♥(C)","♦(D)","♣(T)","♠(P)"]
 
@@ -9,4 +11,17 @@ def combinar(ponderado,simbolos):
     return baraja
 
 baraja = combinar(ponderado,simbolos)
+
+def revolver(baraja):
+    lista = []
+    nuevo_diccionario = {}
+    for i in baraja:
+        lista.append(i)
+    random.shuffle(lista)
+    for i in lista:
+        nuevo_diccionario[i] = baraja[i]
+    return nuevo_diccionario
+
+baraja = revolver(baraja)
+
 print(baraja)
